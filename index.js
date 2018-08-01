a966a2b17e95f98172659c5495246dde5dc116ca
herrifransisca <herrifransisca@gmail.com>
2018-08-02 06:36:44 +0700

    constants

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-// var -> don't use it anymore
-
-let name = 'Mosh';
-console.log(name);
-
-// Cannot be a reserved keyword
-// Should be meaningful
-// Cannot start with a number (1name)
-// Cannot contain a space or hyphen (-)
-// Are case-sensitive
-
-// camel notation used to name variables in javascript
-// let firstName;
-// let FirstName; // it's difference
-
-// multiple variables
-// let firstName, lastName;
-// let firstName = 'Mosh', lastName = 'Hamedani';
-
-// modern best practice is declare each variables on single line
-let firstName = 'Mosh';
-let lastName = 'Hamedani';
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
