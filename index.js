e65c07265e4c513a209e8b61cf03ce18093ccc6c
herrifransisca <herrifransisca@gmail.com>
2018-08-02 06:23:37 +0700

    variables

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-// This is my first JavaScript code!
-console.log("Hello World");
+// var -> don't use it anymore
+
+let name = 'Mosh';
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+// camel notation used to name variables in javascript
+// let firstName;
+// let FirstName; // it's difference
+
+// multiple variables
+// let firstName, lastName;
+// let firstName = 'Mosh', lastName = 'Hamedani';
+
+// modern best practice is declare each variables on single line
+let firstName = 'Mosh';
+let lastName = 'Hamedani';
