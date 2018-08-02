f8b5f2d88127c7190f7fea1c90d0cbae92bf9cd8
herrifransisca <herrifransisca@gmail.com>
2018-08-02 16:58:48 +0700

    primitive types

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const interestRate = 0.3;
-interestRate = 1;
-console.log(interestRate);
+let name = 'Mosh'; // String Literal
+let age = 30; // Number Literal
+let isApproved = true; // Boolean Literal
+
+let firstName; // if we don't initialize it, by default it's value is undefined
+let firstName = undefined; // we could explicitly defined as undefined BUT that's not very common
+
+let selectedColor = null; // is used in situation when we wanna clear the value of variable, e.g. list of colors, if the user has no selection, we set 'null'. in the future if the user selects the color, we reassign this variable to a color like 'red'
