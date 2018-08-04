6166ab83274cc442f1438dd7bb0be4bc2511084d
herrifransisca <herrifransisca@gmail.com>
2018-08-04 21:10:51 +0700

    5- Equality Operators

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-let x = 1;
-
-// Relational Operators
-console.log(x > 0);
-console.log(x >= 1);
-console.log(x < 1);
-console.log(x <= 1);
-
-// Equality Operators
-console.log(x === 1);
-console.log(x !== 1);
+console.log(1 === 1);
+console.log('1' === 1);
