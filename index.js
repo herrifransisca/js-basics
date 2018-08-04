91406e0b1ec59fbb5e71c6815b3eaa3777f273cc
herrifransisca <herrifransisca@gmail.com>
2018-08-04 20:42:56 +0700

    3- Assignment Operators

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 let x = 10;
-let y = 3;
 
-console.log(x + y); // 13 -> addition
-console.log(x - y); // 7 -> substraction
-console.log(x * y); // 30 -> multiplication
-console.log(x / y); // 3.3333333333333335 -> division
-console.log(x % y); // 1 -> remainder of division
-console.log(x ** y); // 1000 -> Exponentiation (10 pangkat 3)
+x++;
+x = x + 1;
 
-// Increment (++)
-console.log(x++); // 10
-console.log(++x); // 12
+x = x + 5;
+x += 5;
 
-// Decrement (--)
-console.log(--x); // 11
-console.log(x--); // 11
+x = x * 3;
+x *= 3;
