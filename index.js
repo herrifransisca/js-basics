c8bd20b2b394df954ba9f930d2a5a9616738dbef
herrifransisca <herrifransisca@gmail.com>
2018-08-04 21:15:25 +0700

    6- Ternary Operator

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log(1 === 1);
-console.log('1' === 1);
+// if a customer has more that 100 points,
+// they are a 'gold' customer, otherwise,
+// they are a 'silver' customer.
+
+let points = 110;
+let type = points > 100 ? 'gold' : 'silver';
+
+console.log(type);
