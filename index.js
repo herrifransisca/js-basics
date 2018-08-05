95d61053b851ed5df3500c10c4c2e351b7aa0153
herrifransisca <herrifransisca@gmail.com>
2018-08-05 23:24:04 +0700

    9- Bitwise Operators

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
-// Falsy (false) values in JavaScript:
-// undefined
-// null
-// 0
-// false
-// ''
-// NaN (not a number)
+// 1        is Decimal Number
+// 00000000 is Binary Number
+// search google with keyword "convert binary to decimal"
 
-// Anything that is not Falsy -> Truthy
+// 1 = 00000001
+// 2 = 00000010
 
-// Short-circuiting
+// 3 = 00000011
+// 0 = 00000000
+
+// 1 | 2
+// result: 00000011 = 3
+// how the result return 11 (last two digits),
+//    herri note/thinking:
+//       0 is false, 1 is true
+//       so -> 0 || 1 = true / 1
+//             1 || 0 = true / 1
+//             result = 11
+console.log(1 | 2); // Bitwise OR -> returns 3 -> result = 00000011
+
+// 1 & 2
+// result: 00000000 = 0
+// how the result return 00 (last two digits),
+//    herri note/thinking:
+//       0 is false, 1 is true
+//       so -> 0 && 1 = false / 0
+//             1 && 0 = false / 0
+//             result = 00
+console.log(1 & 2); // Bitwise AND -> returns 0 -> result = 00000000
 
 // Real-world example:
-let userColor = 'red';
-let defaultColor = 'blue';
-let currentColor = userColor || defaultColor;
-console.log(currentColor); // red
+//    Read, Write, Execute
+//    00000100  -> Read Permission
+//    00000010  -> Write Permission
+//    00000001  -> Execute Permission
+const readPermission = 4;
+const writePermission = 2;
+const executePermission = 1;
 
-userColor = undefined;
-currentColor = userColor || defaultColor;
-console.log(currentColor); // blue
+let myPermission = 0;
+myPermission = myPermission | readPermission | writePermission;
+
+let message = myPermission & readPermission ? 'yes' : 'no';
+console.log(message); // yes
+
+message = myPermission & executePermission ? 'yes' : 'no';
+console.log(message); // no
+
+// Take Away:
+// with Bitwise OR operator, we could ADD PERMISSIONS
+// with Bitwise AND operator, we can CHECK TO SEE IF we have given permission
