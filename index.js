341c61b8e52388fec15deb49a9ab474f33e3ada9
herrifransisca <herrifransisca@gmail.com>
2018-08-06 15:37:57 +0700

    12- Exercise - Swapping Variables

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-let x = 2 + 3 * 4;
-console.log(x); // 14
+let a = 'red';
+let b = 'blue';
+
+console.log(a); // red
+console.log(b); // blue
+
+// exercise, how to swap the value between a and b:
+let c = a;
+a = b;
+b = c;
+
+console.log(a); // blue
+console.log(b); // red
