d36a22725779e56a87c2b4d29f29fa676689f2c3
herrifransisca <herrifransisca@gmail.com>
2018-08-09 15:06:52 +0700

    5- Do...while

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-// i is only accessible in for loop scope
-for (let i = 0; i <= 5; i++) {
-  if (i % 2 !== 0) console.log(i);
-}
-
-// i is different with i above even it has the same name (because in different scope)
+// do-while ALWAYS EXECUTED at least ONCE
 let i = 0;
-while (i <= 5) {
+do {
   if (i % 2 !== 0) console.log(i);
   i++;
-}
+} while (i <= 5);
