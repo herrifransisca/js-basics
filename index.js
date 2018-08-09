1b8a083dce03fa133b0fd1869d9f09c19ec720ed
herrifransisca <herrifransisca@gmail.com>
2018-08-09 14:59:47 +0700

    4- While

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-console.log('Hello World - ugly way');
-console.log('Hello World - ugly way');
-console.log('Hello World - ugly way');
-console.log('Hello World - ugly way');
-console.log('Hello World - ugly way');
+// i is only accessible in for loop scope
+for (let i = 0; i <= 5; i++) {
+  if (i % 2 !== 0) console.log(i);
+}
 
-// using loop
-
-// for (initialExpression; condition; incrementExpression)
-//   single statement
-
-// for (initialExpression; condition; incrementExpression) {
-//   multi statement
-//   multi statement
-// }
-
-// i is convention of 'index' and common convention to use in for loops
-for (let i = 0; i < 5; i++) console.log('Hello World', i);
-
-for (let i = 1; i <= 5; i++) console.log('Hello World', i);
-
-for (let i = 5; i >= 1; i--) console.log('Hello World', i);
-
-for (let i = 0; i < 5; i++) {
-  if (i % 2 !== 0) console.log('Odd Number', i);
+// i is different with i above even it has the same name (because in different scope)
+let i = 0;
+while (i <= 5) {
+  if (i % 2 !== 0) console.log(i);
+  i++;
 }
