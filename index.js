3df8cd08a4d5768da4008aba3e8dd4761eb46215
herrifransisca <herrifransisca@gmail.com>
2018-08-09 15:37:38 +0700

    8- For...of

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-// for
-// while
-// do-while
-
-// for-in
+// use FOR-IN to iterate over the properties of an OBJECT
 const person = {
   name: 'Mosh',
   age: 30
 };
 
 for (let key in person) console.log(key, person[key]);
-// Dot Notation = person.name;
-// Bracket Notation = person['name'];
 
 // for-in is NOT IDEAL for ARRAY
-// for-of is IDEAL for ARRAY (since ECMASCRIPT 6)
 const colors = ['red', 'green', 'blue'];
 for (let index in colors) console.log(index, colors[index]);
+
+// FOR-OF is IDEAL for ARRAY (start from ECMASCRIPT 6/ ES6)
+// use FOR-OF loop to iterate over elements / items in the ARRAY
+for (let color of colors) console.log(color);
