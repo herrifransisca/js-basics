e9f9c734a906f7816d47a48e30bc0c8c2da00d87
herrifransisca <herrifransisca@gmail.com>
2018-08-11 21:59:15 +0700

    16- Exercise 7- String Properties

--- index.js
+++ index.js
@@ -1,70 +1,21 @@
-// Example of Truthy Falsy
-// const isActive = false; // Falsy
-// const isActive = true; // Truthy
-// if (isActive) console.log('Active: ', isActive);
-// // const name = '';  // Falsy (false)
-// const name = 'Mosh'; // Truthy (true)
-// if (name) console.log('Hello', name);
+const movie = {
+  title: 'a',
+  releaseYear: 2018,
+  rating: 4.5,
+  director: 'b'
+};
 
-// Falshy:
-// undefined
-// null
-// ''
-// false
-// 0
-// NaN
+showProperties(movie);
 
-const array = [1, 2, 3, 0, false, NaN, 'mosh', '', undefined, null];
-console.log('Truthy:', countTruthy(array));
-
-// Mosh code:
-function countTruthy(array) {
-  let count = 0;
-  for (let value of array) if (value) count++;
-  return count;
+// Mosh's code:
+function showProperties(obj) {
+  for (let key in obj)
+    if (typeof obj[key] === 'string') console.log(key, obj[key]);
 }
 
-// Note: POOR WAY -> if (value !== undefined || value !== false || ...)
-
-// Herri's code (IT'S NOT WORKING):
-// function countTruthy(array) {
-//   let totalFalsy = 0;
-//   let totalItems = 0;
-
-//   for (let x of array) {
-//     totalItems++;
-
-//     if (typeof x === 'undefined') {
-//       totalFalsy++;
-//       continue;
-//     }
-
-//     if (typeof x === 'null') {
-//       totalFalsy++;
-//       continue;
-//     }
-
-//     if (x === '') {
-//       totalFalsy++;
-//       continue;
-//     }
-
-//     if (x === 0) {
-//       totalFalsy++;
-//       continue;
-//     }
-
-//     if (x === false) {
-//       totalFalsy++;
-//       continue;
-//     }
-
-//     if (isNaN(x)) {
-//       totalFalsy++;
-//     }
+// Herri's code:
+// function showProperties(obj) {
+//   for (properties in obj) {
+//     console.log(properties, obj[properties]);
 //   }
-
-//   console.log('total items', totalItems);
-//   console.log('total falshy', totalFalsy);
-//   return totalItems - totalFalsy;
 // }
