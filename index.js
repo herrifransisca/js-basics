d85194f8c8aaa760fdd36207b19462f89b1deeb9
herrifransisca <herrifransisca@gmail.com>
2018-08-15 22:01:44 +0700

    20- Exercise- Prime Numbers

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
-showStars(5);
+showPrimes(10);
 
-// Mosh's solution: (using NESTED LOOP)
-function showStars(rows) {
-  for (let row = 1; row <= rows; row++) {
-    let pattern = '';
-    for (let i = 1; i <= row; i++) pattern += '*';
-    console.log(pattern);
-  }
+function showPrimes(limit) {
+  for (let number = 2; number <= limit; number++)
+    if (isPrime(number)) console.log(number);
 }
 
+function isPrime(number) {
+  for (let factor = 2; factor < number; factor++)
+    if (number % factor === 0) return false;
+
+  return true;
+}
+
+// Before Refactoring - making more simple:
+// function isPrime(number) {
+//   let isPrime = true;
+//   for (let factor = 2; factor < number; factor++)
+//     if (number % factor === 0) {
+//       isPrime = false;
+//       break;
+//     }
+
+//   return isPrime;
+// }
+
+// Mosh's code (before refactoring - doing single thing)
+// function showPrimes(limit) {
+//   for (let number = 2; number <= limit; number++) {
+//     let isPrime = true;
+//     for (let factor = 2; factor < number; factor++)
+//       if (number % factor === 0) {
+//         isPrime = false;
+//         break;
+//       }
+
+//     if (isPrime) console.log(number);
+//   }
+// }
+
 // Herri's code:
-// function showStars(rows) {
-//   let stars = '';
-//   for (let i = 0; i < rows; i++) {
-//     stars += '*';
-//     console.log(stars);
+// function showPrimes(limit) {
+//   for (let i = 1; i <= limit; i++) {
+//     let count = 0;
+//     for (let x = 1; x <= i; x++) if (i % x === 0) count++;
+
+//     if (count === 2) console.log(i);
 //   }
 // }
