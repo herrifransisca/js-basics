9bf36f05c228d6059f90366300b334dfd284f4fb
herrifransisca <herrifransisca@gmail.com>
2018-08-22 18:43:06 +0700

    2- Factory Functions

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-// let radius = 1;
-// let x = 1;
-// let y = 1;
+function createCircle(radius) {
+  return {
+    radius,
+    draw() {
+      console.log('draw');
+    }
+  };
+}
 
-// Object Oriented Programming (OOP)
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1
-  },
-  isVisible: true,
-  draw: function() {
-    console.log('draw');
-  }
-};
-
-// function draw() {}
-// function move() {}
-
-circle.draw(); // Method
+const circle1 = createCircle(1);
+console.log(circle1);
