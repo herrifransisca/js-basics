fea0399c998fe891f268a449d47fd90cae5b3c71
herrifransisca <herrifransisca@gmail.com>
2018-09-08 23:24:07 +0700

    10- Garbage Collection

--- index.js
+++ index.js
@@ -1,22 +1,2 @@
-const circle = {
-  radius: 1,
-  draw() {
-    console.log('draw');
-  }
-};
-
-// 1st way
-// const another = {};
-// for (let key in circle) another[key] = circle[key];
-// Notes:
-//    another[key] = circle[key];
-//    equavalent to :
-//    another['radius'] = circle['radius'];
-
-// 2nd way - Object.assign()
-// const another = Object.assign({}, circle);
-
-// 3rd way - spread operator
-const another = { ...circle };
-
-console.log(another);
+let circle = {};
+console.log(circle);
