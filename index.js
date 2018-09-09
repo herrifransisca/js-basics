22fe069f26f928cf76857c361ad639dbe47f28fe
herrifransisca <herrifransisca@gmail.com>
2018-09-09 18:27:52 +0700

    12- String

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-console.log(Math.PI);
+// String primitive
+let message = 'This is my first message';
 
-console.log(Math.random());
-console.log(Math.random());
-console.log(Math.random());
+// String object
+const another = new String('hi');
 
-const min = 2;
-const max = 4;
-console.log(Math.random(max - min) - min);
+console.log(message.length); // 23
 
-console.log(Math.round(1.9)); // 2
-console.log(Math.round(1.4)); // 1
-console.log(Math.round(1.5)); // 2
+console.log(message[0]); // T
+console.log(message[1]); // h
 
-console.log(Math.max(1, 2, 3, 4, 5)); // 5
-console.log(Math.min(1, 2, 3, 4, 5)); // 1
+console.log(message.includes('my')); // true
+console.log(message.includes('not')); // false
 
-const numbers = [1, 2, 3, 4, 5];
-console.log(Math.max(...numbers)); // 5
+console.log(message.startsWith('This')); // true
+console.log(message.startsWith('this')); // false
+
+console.log(message.endsWith('e')); // true
+
+console.log(message.indexOf('my')); // 8
+
+console.log(message.replace('first', 'second')); // This is my second message
+// Note that this RETURNS A NEW STRING and DOESN’T REPLACE THE ORIGINAL ONE
+console.log(message); // This is my first message
+
+console.log(message.toUpperCase());
+console.log(message.toLowerCase());
+
+message = ' This is my first message ';
+console.log(message.trim());
+console.log(message.trimLeft());
+console.log(message.trimRight());
+
+console.log(message.split(' ')); // (7) ["", "this", "is", "my", "first", "message", ""]
+
+message = "This is my 'first message";
+console.log(message); // This is my 'first message
+
+message = 'This is my\n first message';
+console.log(message); // This is my
+//  First message
