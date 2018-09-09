ddb38c401bc8b499a7d13362a35487500b67695e
herrifransisca <herrifransisca@gmail.com>
2018-09-09 22:23:39 +0700

    15- Exercise 1- Address Object

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const now = new Date();
-const date1 = new Date('May 11 2018 09:00');
-const date2 = new Date(2018, 4, 11, 9);
+const address = {
+  street: 'Komplek Handil Lestari No. B15',
+  city: 'Jambi',
+  zipCode: '36137'
+};
 
-now.getDate();
-now.setFullYear(2017);
+function showAddress(address) {
+  for (let key in address) console.log(key, address[key]);
+}
 
-console.log(now.toDateString()); // Sat Sep 09 2017
-console.log(now.toTimeString()); // 20:10:29 GMT+0700 (Western Indonesia Time)
-console.log(now.toISOString()); // 2017-09-09T13:10:29.716Z
+showAddress(address);
