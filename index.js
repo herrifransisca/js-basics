25ddb67ad3e0f110d42bcb7b384f4661093548f8
herrifransisca <herrifransisca@gmail.com>
2018-09-10 22:44:42 +0700

    16- Exercise 2- Factory and Constructor Functions

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-const address = {
-  street: 'Komplek Handil Lestari No. B15',
-  city: 'Jambi',
-  zipCode: '36137'
-};
+// Herri's Solution
 
-function showAddress(address) {
-  for (let key in address) console.log(key, address[key]);
+// // Factory Function
+// function getAddress() {
+//   return {
+//     street: 'a',
+//     city: 'b',
+//     zipCode: 'c'
+//   };
+// }
+
+// // Constructor Function
+// function Address() {
+//   this.street = 'a';
+//   this.city = 'b';
+//   this.zipCode = 'c';
+// }
+
+// function showAddress(address) {
+//   for (let key in address) console.log(key, address[key]);
+// }
+
+// const address = getAddress();
+// showAddress(address);
+
+// const anotherAddress = new Address();
+// showAddress(anotherAddress);
+
+// Mosh's Solution
+
+const address = createAddress('a', 'b', 'c');
+console.log(address);
+
+const anotherAddress = new Address('a', 'b', 'c');
+console.log(anotherAddress);
+
+// Factory Function
+function createAddress(street, city, zipCode) {
+  return {
+    street,
+    city,
+    zipCode
+  };
 }
 
-showAddress(address);
+// Constructor Function
+function Address(street, city, zipCode) {
+  this.street = street;
+  this.city = city;
+  this.zipCode = zipCode;
+}
