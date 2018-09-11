323b32a23e3c4a5150228d67e694bbb474f23bc8
herrifransisca <herrifransisca@gmail.com>
2018-09-11 14:06:50 +0700

    19- Exercise 5- Constructor Functions

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 // Mosh's Code
-const post = {
-  title: 'a',
-  body: 'b',
-  author: 'c',
-  views: 10,
-  comments: [{ author: 'a', body: 'b' }, { author: 'c', body: 'd' }],
-  isLive: true
-};
+
+const post = new Post('a', 'b', 'c');
+
+function Post(title, body, author) {
+  this.title = title;
+  this.body = body;
+  this.author = author;
+  this.views = 0;
+  this.comments = [];
+  this.isLive = false;
+}
+
+console.log(post);
 
 // Herri's Code
-const blogPost = {
-  title: 'a',
-  body: 'b',
-  author: 'c',
-  views: '1',
-  comments: {
-    author: 'author-comment',
-    body: 'author-body'
-  },
-  live: true
-};
+// const post = new Post(
+//   'a',
+//   'b',
+//   'c',
+//   10,
+//   [{ author: 'a', body: 'b' }, { author: 'c', body: 'd' }],
+//   true,
+//   false
+// );
+
+// function Post(title, body, author, views, comments, isLive, isPublished) {
+//   this.title = title;
+//   this.body = body;
+//   this.author = author;
+//   this.views = views;
+//   this.comments = comments;
+//   this.isLive = isLive;
+//   this.isPublished = isPublished;
+// }
+
+// console.log(post);
