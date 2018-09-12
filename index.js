f51e36b7df94c3ec6ae7099ecc1b403cedbd0f6b
herrifransisca <herrifransisca@gmail.com>
2018-09-12 23:38:26 +0700

    2- Adding Elements

--- index.js
+++ index.js
@@ -1,36 +1,12 @@
-// Mosh's solution
-const priceRanges = [
-  { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10 },
-  { label: '$$', tooltip: 'Moderate', minPerPerson: 11, maxPerPerson: 20 },
-  { label: '$$$', tooltip: 'Expensive', minPerPerson: 21, maxPerPerson: 50 }
-];
+const numbers = [3, 4];
 
-const restaurants = [{ averagePerPerson: 5 }];
+// End
+numbers.push(5, 6);
 
-// Herri's solution
-// const priceRanges = [new PriceRange(1), new PriceRange(2), new PriceRange(3)];
+// Beginning
+numbers.unshift(1, 2);
 
-// console.log(priceRanges);
+// Middle
+numbers.splice(2, 0, 'a', 'b');
 
-// function PriceRange(level) {
-//   if (level === 1) {
-//     this.priceName = 'Inexpensive';
-//     this.displayedName = '$';
-//     this.minPrice = 1;
-//     this.maxPrice = 4;
-//   }
-
-//   if (level === 2) {
-//     this.priceName = 'Moderate';
-//     this.displayedName = '$$';
-//     this.minPrice = 5;
-//     this.maxPrice = 7;
-//   }
-
-//   if (level === 3) {
-//     this.priceName = 'Pricey';
-//     this.displayedName = '$$$';
-//     this.minPrice = 8;
-//     this.maxPrice = 10;
-//   }
-// }
+console.log(numbers);
