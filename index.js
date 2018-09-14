763f2da70643a3248118dc77d8c6f6be687d9ee8
herrifransisca <herrifransisca@gmail.com>
2018-09-14 21:24:11 +0700

    3- Finding Elements (Primitives)

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const numbers = [3, 4];
+const numbers = [1, 2, 3, 1, 4];
 
-// End
-numbers.push(5, 6);
+// #1 - if exist
+console.log(numbers.indexOf(1)); // 0
 
-// Beginning
-numbers.unshift(1, 2);
+// #2 - note the "type" (string - number)
+console.log(numbers.indexOf('1')); // -1
 
-// Middle
-numbers.splice(2, 0, 'a', 'b');
+// #3 - if doesn't exist
+console.log(numbers.indexOf('a')); // -1
 
-console.log(numbers);
+// #4 - little bit ugly
+console.log(numbers.indexOf(1) !== -1); // true
+
+// #5 - a new method achieving the same thing
+console.log(numbers.includes(1)); // true
+
+// #6 - if exist
+console.log(numbers.lastIndexOf(1)); // 3
+
+// #7 - if doesn't exist
+console.log(numbers.lastIndexOf(5)); // -1
+
+// #8 - before using SECOND PARAMETER, fromINDEX
+console.log(numbers.indexOf(1)); // 0
+
+// #9 - after using SECOND PARAMETER, fromINDEX
+console.log(numbers.indexOf(1, 2)); // 3
