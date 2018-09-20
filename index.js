e42f37f181db2291f3bec63d52aee9194bc3f3ef
herrifransisca <herrifransisca@gmail.com>
2018-09-20 20:24:40 +0700

    8- Combining and Slicing Arrays

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-let numbers = [1, 2, 3, 4];
-let another = numbers;
+let first = [1, 2, 3];
+const second = [4, 5, 6];
 
-// Sol;ution 1
-// numbers = [];
-// console.log(numbers); // []
-// console.log(another); // [1, 2, 3, 4]
+let combined = first.concat(second); // [1, 2, 3, 4, 5, 6]
 
-// Solution 2
-// numbers.length = 0;
-// console.log(numbers); // []
-// console.log(another); // []
+// 1st way to use slice()
+// const slice = combined.slice(2, 4); // [3, 4]
 
-// Solution 3
-// numbers.splice(0, numbers.length);
-// console.log(numbers); // []
-// console.log(another); // []
+// 2nd way to use slice()
+// const slice = combined.slice(2); // [3, 4, 5, 6];
 
-// Solution 4
-let i = 0;
-while (numbers.length > 0) numbers.pop();
-console.log(numbers); // []
-console.log(another); // []
+// 3rd way to use slice()
+const slice = combined.slice(); // [1, 2, 3, 4, 5, 6]
+
+console.log(combined);
+console.log(slice);
+
+/* 
+One thing that you need to understand about both of these method ( concat() and slice() ) is that :
+	
+  If you're dealing with PRIMITIVE VALUES, this primitive would be copied into the target array
+
+  If you HAVE OBJECTS in your array, object themselves are not copied, their references are copied 
+  And that means the element in both the input and output arrays would point to the same object.
+*/
+
+first = [{ id: 1 }];
+combined = first.concat(second);
+first[0].id = 10;
+console.log(combined); // [ {id: 10}, 4, 5, 6]
