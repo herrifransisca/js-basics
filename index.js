5590b1c8598139c5d517fa7fdca3d1bd94790073
herrifransisca <herrifransisca@gmail.com>
2018-09-22 16:43:52 +0700

    13- Testing the Elements of an Array

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
-let courses = [{ id: 1, name: 'Node.js' }, { id: 2, name: 'JavaScript' }];
+let numbers = [1, 2, 3];
 
-// do nothing
-// courses.sort();
+const allPositive = numbers.every(number => number >= 0);
 
-courses.sort(function(a, b) {
-  if (a.name < b.name) return -1;
-  if (a.name > b.name) return 1;
-  return 0;
-});
+console.log(allPositive); // true
 
-console.log(courses); // [{ id: 2, name: 'JavaScript' }, { id: 1, name: 'Node.js'}]
+numbers = [1, -1, 2, 3];
+const allPositive2 = numbers.every(number => number >= 0);
+console.log(allPositive2); // false
 
-// change the 'J' to lowercase
-courses = [{ id: 1, name: 'Node.js' }, { id: 2, name: 'javaScript' }];
-
-// javascript becomes no. 2, below node.js
-console.log(courses); // [{ id: 1, name: 'Node.js' }, { id: 2, name: 'javaScript' }];
-
-// To solve this problem, we should EXCLUDE CASE SENSITIVITY when COMPARING these names.
-// Both these names should be either lowercase or uppercase.
-courses.sort((a, b) => {
-  const nameA = a.name.toLowerCase();
-  const nameB = b.name.toLowerCase();
-
-  if (nameA < nameB) return -1;
-  if (nameA > nameB) return 1;
-  return 0;
-});
-
-// javascript becomes no. 1 AGAIN, above node.js
-console.log(courses); // [{ id: 2, name: 'javaScript' }, { id: 1, name: 'Node.js'}]
+const atLeastOnePositive = numbers.some(number => number >= 0);
+console.log(atLeastOnePositive); // true
