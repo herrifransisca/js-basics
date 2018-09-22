d30d54eb745a3c40632cf4e3e9c70ebb1d070e83
herrifransisca <herrifransisca@gmail.com>
2018-09-22 15:55:38 +0700

    12- Sorting Arrays

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-const numbers = [1, 2, 3];
+let courses = [{ id: 1, name: 'Node.js' }, { id: 2, name: 'JavaScript' }];
 
-const joined = numbers.join(',');
-console.log(joined); // 1,2,3
+// do nothing
+// courses.sort();
 
-const message = 'This is my first message';
-const parts = message.split(' ');
-console.log(parts); // ['This', 'is', 'my', 'first', 'message']
+courses.sort(function(a, b) {
+  if (a.name < b.name) return -1;
+  if (a.name > b.name) return 1;
+  return 0;
+});
 
-const combined = parts.join('-');
-console.log(combined); // This-is-my-first-message
+console.log(courses); // [{ id: 2, name: 'JavaScript' }, { id: 1, name: 'Node.js'}]
+
+// change the 'J' to lowercase
+courses = [{ id: 1, name: 'Node.js' }, { id: 2, name: 'javaScript' }];
+
+// javascript becomes no. 2, below node.js
+console.log(courses); // [{ id: 1, name: 'Node.js' }, { id: 2, name: 'javaScript' }];
+
+// To solve this problem, we should EXCLUDE CASE SENSITIVITY when COMPARING these names.
+// Both these names should be either lowercase or uppercase.
+courses.sort((a, b) => {
+  const nameA = a.name.toLowerCase();
+  const nameB = b.name.toLowerCase();
+
+  if (nameA < nameB) return -1;
+  if (nameA > nameB) return 1;
+  return 0;
+});
+
+// javascript becomes no. 1 AGAIN, above node.js
+console.log(courses); // [{ id: 2, name: 'javaScript' }, { id: 1, name: 'Node.js'}]
