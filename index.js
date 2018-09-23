7eee4e7b5981dadb7eb0838dd7bd65c9acfa8498
herrifransisca <herrifransisca@gmail.com>
2018-09-23 21:46:40 +0700

    15- Mapping an Array

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 const numbers = [1, -1, 2, 3];
 
-// const filtered = numbers.filter(function(value) {
-//   return value >= 0;
-// });
+/* 1- mapping this numbers to string */
+// // const filtered = numbers.filter(n => n >= 0); // [1, 2, 3]
 
-// arrow function
-// const filtered = numbers.filter(value => value >= 0);
+// // const items = filtered.map(n => '<li>' + n + '</li>'); // ["<li>1</li>", "<li>2</li>", "<li>3</li>"]
 
-// we can make this code a little bit shorter
-const filtered = numbers.filter(n => n >= 0);
+// // const html = '<ul>' + items.join('') + '</ul>';
 
-console.log(filtered);
+// // console.log(html);
+
+/* 2- mapping this number to object */
+// const filtered = numbers.filter(n => n >= 0); // [1, 2, 3]
+
+// // const items = filtered.map(n => {
+// //   const obj = { value: n };
+// //   return obj;
+// // });
+
+// // const items = filtered.map(n => {
+// //   return { value: n };
+// // });
+
+/* If you have a single line of code and you returning a value, you can exclude the return keyword, as well as the curly braces */
+// // const items = filtered.map(n => {
+// //   value: n;
+// // });
+
+/* So if you are returning an object, you need to put that object in parenthesis like this */
+// const items = filtered.map(n => ({ value: n }));
+
+/* Chaining */
+// const items = numbers
+//   .filter(n => n >= 0)
+//   .map(n => ({ value: n }));
+
+/* We can call the filter() one more time */
+// const items = numbers
+//   .filter(n => n >= 0)
+//   .map(n => ({ value: n }))
+//   .filter(obj => obj.value > 1);
+
+/* And again we can call a map() */
+const items = numbers
+  .filter(n => n >= 0)
+  .map(n => ({ value: n }))
+  .filter(obj => obj.value > 1)
+  .map(obj => obj.value);
+
+console.log(items);
