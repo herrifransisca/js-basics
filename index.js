117aed53cbba621012946cfc4429a2fd6ffb0c20
herrifransisca <herrifransisca@gmail.com>
2018-09-25 18:19:30 +0700

    19- Exercise 3- Except

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const numbers = [1, 2, 3, 4];
+const numbers = [1, 2, 3, 4, 1, 1];
 
-// console.log(numbers.includes(1));
-console.log(includes(numbers, 1));
+const output = except(numbers, [1, 4]);
+
+console.log(output);
 
 // mosh's solution
-function includes(array, searchElement) {
-  for (let element of array) if (element === searchElement) return true;
-  return false;
+function except(array, excluded) {
+  const output = [];
+  for (let element of array)
+    if (!excluded.includes(element)) output.push(element);
+  return output;
 }
 
 // herri's solution
-function includes(array, searchElement) {
-  for (let i of array) if (i === searchElement) return true;
-  return false;
-}
+// function except(array, excluded) {
+//   const numbers = [];
+//   for (let i of array)
+//     if (!excluded.includes(i))
+//       numbers.push(i);
+//   return numbers;
+// }
