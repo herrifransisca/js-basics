1a46d6bd6525d1d63b571c1bf96d2f440e181b3f
herrifransisca <herrifransisca@gmail.com>
2018-09-25 16:29:17 +0700

    16- Reducing an Array

--- index.js
+++ index.js
@@ -1,50 +1,29 @@
 const numbers = [1, -1, 2, 3];
 
-/* 1- mapping this numbers to string */
-// // const filtered = numbers.filter(n => n >= 0); // [1, 2, 3]
+// for of loop
+// let sum = 0;
+// for (let number of numbers) sum += number;
 
-// // const items = filtered.map(n => '<li>' + n + '</li>'); // ["<li>1</li>", "<li>2</li>", "<li>3</li>"]
+// reduce() - supply an initial value
+// accumulator = 0, currentValue = 1 => accumulator = 1
+// accumulator = 1, currentValue = -1 => accumulator = 0
+// accumulator = 0, currentValue = 2 => accumulator = 2
+// accumulator = 2, currentValue = 3 => accumulator = 5
+// const sum = numbers.reduce((accumulator, currentValue) => {
+//   return accumulator + currentValue;
+// }, 0);
 
-// // const html = '<ul>' + items.join('') + '</ul>';
+// reduce - exclude the initialization of the accumulator
+// accumulator = 1, currentValue = -1 => accumulator = 0
+// accumulator = 0, currentValue = 2 => accumulator = 2
+// accumulator = 2, currentValue = 3 => accumulator = 5
+// const sum = numbers.reduce((accumulator, currentValue) => {
+//   return accumulator + currentValue;
+// });
 
-// // console.log(html);
+// get rid of this return keyword
+const sum = numbers.reduce(
+  (accumulator, currentValue) => accumulator + currentValue
+);
 
-/* 2- mapping this number to object */
-// const filtered = numbers.filter(n => n >= 0); // [1, 2, 3]
-
-// // const items = filtered.map(n => {
-// //   const obj = { value: n };
-// //   return obj;
-// // });
-
-// // const items = filtered.map(n => {
-// //   return { value: n };
-// // });
-
-/* If you have a single line of code and you returning a value, you can exclude the return keyword, as well as the curly braces */
-// // const items = filtered.map(n => {
-// //   value: n;
-// // });
-
-/* So if you are returning an object, you need to put that object in parenthesis like this */
-// const items = filtered.map(n => ({ value: n }));
-
-/* Chaining */
-// const items = numbers
-//   .filter(n => n >= 0)
-//   .map(n => ({ value: n }));
-
-/* We can call the filter() one more time */
-// const items = numbers
-//   .filter(n => n >= 0)
-//   .map(n => ({ value: n }))
-//   .filter(obj => obj.value > 1);
-
-/* And again we can call a map() */
-const items = numbers
-  .filter(n => n >= 0)
-  .map(n => ({ value: n }))
-  .filter(obj => obj.value > 1)
-  .map(obj => obj.value);
-
-console.log(items);
+console.log(sum);
