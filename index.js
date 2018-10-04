3e9dc62771cd0543a2a88cc87665b406f7d5f04d
herrifransisca <herrifransisca@gmail.com>
2018-10-04 22:38:28 +0700

    10- The this Keyword

--- index.js
+++ index.js
@@ -1,62 +1,122 @@
-// 	Use "let keyword"
-function start() {
-  for (let i = 0; i < 5; i++) console.log(i);
+// 1st RULE: method -> obj
+// 2nd RULE: function -> global (window, global)
 
-  console.log(i); // error
-}
-
-start();
-
-// Change "let" to "VAR"
-function start() {
-  for (var i = 0; i < 5; i++) console.log(i);
-
-  console.log(i); // 0 | 1 | 2 | 3 | 4 | 5
-}
-
-start();
-
-// 	Let's take a look at another example:
-//  "var" -> color is accessible
-function start() {
-  for (var i = 0; i < 5; i++) {
-    if (true) {
-      var color = 'red';
-    }
+// Let's take a look at examples of 1st rule
+const video = {
+  title: 'a',
+  play() {
+    console.log(this);
   }
+};
 
-  console.log(color); // red
+video.play();
+
+video.stop = function() {
+  console.log(this);
+};
+
+video.stop();
+
+// Let's take a look at examples of 2nd rule
+// "this" -> a global object which is "window in browser" and "global in node"
+function playVideo() {
+  console.log(this); // Window { ... }
 }
 
-start();
+playVideo();
 
-//  "let" -> color is NOT accessible
-//  THAT'S HOW MOST LANGUAGES WORK
-function start() {
-  for (var i = 0; i < 5; i++) {
-    if (true) {
-      let color = 'red';
-    }
+// what if "this" IS CONSTRUCTOR FUNCTION ?
+// instead of "window object", we get "a new / this video object"
+function Video(title) {
+  this.title = title;
+  console.log(this); // Video {title: 'b'}
+}
+
+const v = new Video('b');
+
+// Last example
+const video = {
+  title: 'a',
+  tags: ['a', 'b', 'c'],
+  showTags() {
+    this.tags.forEach(function(tag) {
+      console.log(tag); // 'a'
+    });
   }
+};
 
-  console.log(color); // error
-}
+video.showTags();
 
-start();
+// 	What if we wanna display the "title of the video" "next to each tag" ?
+//		WE GOT UNDEFINED, WHAT'S GOING ON HERE ?
+const video = {
+  title: 'a',
+  tags: ['a', 'b', 'c'],
+  showTags() {
+    this.tags.forEach(function(tag) {
+      console.log(this.title, tag); // undefined, 'a'
+    });
+  }
+};
 
-// the second issue is WITH GLOBAL VARIABLES
-// The "VAR" keyword attaches this color variable to the window object.
-// window.color -> 'red'
-var color = 'red';
-let age = 30;
+video.showTags();
 
-// the "let keyword" to define a global variable,
-// that global variable is NOT ATTACH to the WINDOW OBJECT
-// window.age -> undefined
+// 		Let's remove the "title property" and see WHAT "THIS" is referencing!
+//    IT'S REFERENCING THE WINDOW OBJECT
+const video = {
+  title: 'a',
+  tags: ['a', 'b', 'c'],
+  showTags() {
+    this.tags.forEach(function(tag) {
+      console.log(this, tag); // window{...}, 'a'
+    });
+  }
+};
 
-// 	So technically it's a global function, attached to the window object and :
-// 	THAT IS BAD PRACTICE. But how can we PREVENT THIS ?
-// window.sayHi() -> 'hi'
-function sayHi() {
-  console.log('hi');
-}
+video.showTags();
+
+/* HOW CAN WE SOLVE THIS PROBLEM AND DISPLAY "THE TITLE OF THE VIDEO" next to each tag ?
+In this particular case, the "forEach()" has TWO PARAMETERS:
+1st Parameter -> a call back function
+2nd Parameter -> thisArg  */
+const video = {
+  title: 'a',
+  tags: ['a', 'b', 'c'],
+  showTags() {
+    this.tags.forEach(
+      function(tag) {
+        console.log(this, tag); // { firstName: 'Mosh' }, 'a'
+      },
+      { firstName: 'Mosh' }
+    );
+  }
+};
+
+video.showTags();
+
+/* In this example, we don't really want "this object" ( { firstName: 'Mosh' } ),
+WE WANT A VIDEO OBJECT so we can pass "this" here, */
+const video = {
+  title: 'a',
+  tags: ['a', 'b', 'c'],
+  showTags() {
+    this.tags.forEach(function(tag) {
+      console.log(this, tag); // { title: 'a', tags: Array(3), showTags: f }, 'a'
+    }, this);
+  }
+};
+
+video.showTags();
+
+// We can add "title property" here
+const video = {
+  title: 'a',
+  tags: ['a', 'b', 'c'],
+  showTags() {
+    this.tags.forEach(function(tag) {
+      console.log(this.title, tag); // a a | a b | a c
+    }, this);
+  }
+};
+
+video.showTags();
